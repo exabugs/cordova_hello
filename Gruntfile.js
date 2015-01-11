ce0f374fff60a24324e1c246932f3973282cc326
exabugs <exabugs@gmail.com>
2015-01-11 12:47:20 +0900

    first commit.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,16 +14,93 @@ module.exports = function(grunt) {
           }
         }
       }
+    },
+
+    watch: {
+      jade: {
+        tasks: 'jade',
+        files: ['src/**/*.jade']
+      },
+      scss: {
+        tasks: 'compass',
+        files: ['src/**/*.scss']
+      },
+      js: {
+        tasks: 'uglify',
+        files: ['src/**/*.js']
+      }
+    },
+
+    copy: {
+      img: {
+        expand: true,
+        cwd: 'src/img',
+        src: '*',
+        dest: 'www/img/',
+        filter: 'isFile'
+      }
+    },
+
+    compass: {
+      dist: {
+        options: {
+          relativeAssets: true,
+          httpPath: '/',
+          sassDir: 'src/css',
+          cssDir: 'www/css',
+          imagesDir: 'src/img',
+          generatedImagesDir: 'www/img',
+          javascriptsDir: 'js'
+        }
+      }
+    },
+
+    uglify: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/js/',
+          src: '*.js',
+          dest: 'www/js/',
+          ext: '.min.js'
+        }]
+      }
+    },
+
+    jade: {
+      compile: {
+        options: {
+          pretty: true //htmlをインデント表記させる
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: [
+            '*.jade',
+            '!_*.jade'
+          ],
+          dest: 'www/',
+          ext: '.html'
+        }]
+      }
     }
+
+
   });
 
   grunt.loadNpmTasks('grunt-npm-install');
   grunt.loadNpmTasks('grunt-bower-install');
   grunt.loadNpmTasks('grunt-bower-task');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('default', [
     'npm-install',
-    'bower'
+    'bower',
+    'copy'
   ]);
 
 };
